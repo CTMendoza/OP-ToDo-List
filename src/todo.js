e644cd966fa433e14b51074f5f6aa0427fec09f0
Christopher Mendoza <tiptop1112@gmail.com>
2024-12-12 13:14:12 -0800

    Created todo.js file Created the todo.js file as a component that creates new todos objects based on information submitted by the user

---
+++ src/todo.js
@@ -0,0 +1,10 @@
+//this component creates new todos objects based on user entries on the web app
+
+class todos {
+    constructor (title, description,dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
