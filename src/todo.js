35943c886f7e9e7fb2e3d104ffbcb6cb82a80b00
Christopher Mendoza <tiptop1112@gmail.com>
2024-12-16 13:59:27 -0800

    Edited template.html Added a couple of div elements and grid styling to visualize the web page and how content will fit into it

--- src/todo.js
+++ src/todo.js
@@ -1,10 +1,11 @@
 //this component creates new todos objects based on user entries on the web app
 
 class todos {
-    constructor (title, description,dueDate, priority) {
+    constructor (title, description,dueDate, priority,notes) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.notes = notes;
     }
 }
