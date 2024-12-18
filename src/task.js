156d9b6001e252a36f5250cac552288d2562b85a
Christopher Mendoza <tiptop1112@gmail.com>
2024-12-18 10:38:09 -0800

    Added html elements and styling to the .sidebar element Added buttons to create task and projects and styled them using pseudo-classes for when a user clicks and hovers over them.

--- src/task.js
+++ src/task.js
@@ -1,6 +1,6 @@
-//this component creates new todos objects based on user entries on the web app
+//this component creates new task objects based on user entries on the web app
 
-class todos {
+class task {
     constructor (title, description,dueDate, priority,notes) {
         this.title = title;
         this.description = description;
