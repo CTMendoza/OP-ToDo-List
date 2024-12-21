66b590e95e22ff09e796e613d7f286802d41b540
Christopher Mendoza <tiptop1112@gmail.com>
2024-12-21 11:42:39 -0800

    Added a "close button" to the modal The modal now has a close button so that the user that can close and exit the modal

--- src/toggleModals.js
+++ src/toggleModals.js
@@ -1,11 +1,19 @@
 export {setTaskModalListener, setProjectModalListener}
 
+
+
 function setTaskModalListener () {
+    const close = document.querySelector('.close');
     const taskModal = document.querySelector('#task-modal');
     const createTaskButton = document.querySelector('.new-task-bttn');
+    
     createTaskButton.addEventListener('click', () => {
-    taskModal.style.display = "block";
-})
+        taskModal.style.display = "block";
+    })   
+     
+    close.addEventListener('click', () => {
+        taskModal.style.display = 'none';
+    })
 }
 
 function setProjectModalListener () {
