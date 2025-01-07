1adf43e9dcb91e48ee00d18e639714a6eb67f237
Christopher Mendoza <tiptop1112@gmail.com>
2025-01-07 12:36:24 -0800

    Fixing merging problems pt 2

--- src/localStorage.js
+++ src/localStorage.js
@@ -17,18 +17,11 @@ function saveTasksToLocalStorage (newTask) {
 }
 
 function loadTasksFromLocalStorage (taskList) {
-<<<<<<< HEAD
    // reset taskList to having no child elements so that child elements are not rendered twice
     taskList.innerHTML = '';
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     console.log('Loaded Tasks:', tasks);
     // render tasks on the UI
-=======
-    taskList.innerHTML = '';
-    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    console.log('Loaded Tasks:', tasks);
-    // Optionally, render tasks on the UI
->>>>>>> 920d8c0b5cf050439c118a9c3d2ef246d96a0a9d
     tasks.forEach((task, index) => {
         const taskItem = document.createElement('li');
         taskItem.className='task-item';
@@ -36,7 +29,6 @@ function loadTasksFromLocalStorage (taskList) {
         // Set a unique data-task-id for each taskItem
         taskItem.setAttribute('data-task-id', index + 1);
 
-<<<<<<< HEAD
         const deleteButton = document.createElement('div');
         deleteButton.className = 'ion--trash';
 
@@ -44,23 +36,16 @@ function loadTasksFromLocalStorage (taskList) {
         taskItemLine1.className ='task-item-line1'
         const taskItemLine2 = document.createElement('div');
         taskItemLine2.className ='task-item-line2'
-=======
-        const taskItemLine1 = document.createElement('div');
-        const taskItemLine2 = document.createElement('div');
->>>>>>> 920d8c0b5cf050439c118a9c3d2ef246d96a0a9d
 
         const taskTitle = document.createElement('h2');
         taskTitle.className = 'title';
         taskTitle.textContent = `${task.title}`
 
-<<<<<<< HEAD
         const checkBox = document.createElement('input');
         checkBox.type = 'checkbox';
         checkBox.name = 'complete';
         checkBox.className = 'complete'
 
-=======
->>>>>>> 920d8c0b5cf050439c118a9c3d2ef246d96a0a9d
         const taskDate = document.createElement('h3');
         taskDate.className = 'due-date'
         taskDate.textContent = `${task.dueDate}`
@@ -77,13 +62,9 @@ function loadTasksFromLocalStorage (taskList) {
 
         taskItem.appendChild(taskItemLine1);
         taskItem.appendChild(taskItemLine2);
-<<<<<<< HEAD
         taskItemLine1.appendChild(checkBox);
         taskItemLine1.appendChild(taskTitle);
         taskItemLine2.appendChild(deleteButton);
-=======
-        taskItemLine1.appendChild(taskTitle);
->>>>>>> 920d8c0b5cf050439c118a9c3d2ef246d96a0a9d
         taskItemLine2.appendChild(taskDate);
         taskItemLine2.appendChild(taskPriority);
 
