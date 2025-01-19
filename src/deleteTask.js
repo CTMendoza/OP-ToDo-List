a066d65528fb29767f20adad11ad832b890b4125
Christopher Mendoza <tiptop1112@gmail.com>
2025-01-19 10:19:10 -0800

    Added delete task functionality to completed task The ability to delete task now covers task that are marked as completed. Current task and completed task are stored in two different arrays within localstorage. So when the delete key is pressed, there are checks to see if the task sits within the completed task ul element or the current task ul element

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -7,10 +7,21 @@ function deleteTask(element) {
             const parentLi = event.target.closest('li');
             const taskId = parentLi.getAttribute('data-task-id');
             setSelectedTaskId(taskId);
-            const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-            const taskIndex = Number(selectedTaskId);
-            tasks.splice(taskIndex, 1);
-            localStorage.setItem('tasks', JSON.stringify(tasks));
+            const parentUl = event.target.closest('ul')
+            const ulClassName =parentUl.className;
+
+            if (ulClassName.includes('completed-tasks-list')) {
+                const completedTasks = JSON.parse(localStorage.getItem('completedTasks')) || [];
+                const completedTasksIndex = Number(selectedTaskId);
+                completedTasks.splice(completedTasksIndex, 1);
+                localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
+            } else {
+                const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+                const taskIndex = Number(selectedTaskId);
+                tasks.splice(taskIndex, 1);
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+            }
+            
             parentLi.remove();
             resetSelectedTaskId();
         }
